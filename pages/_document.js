1f1d72574c6517ab8ea1074d2c025e6cf8a5a1c9
jack-dev11 <110835112+jack-dev11@users.noreply.github.com>
2023-08-09 14:07:16 +0100

    seo updates

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond&family=EB+Garamond:ital,wght@0,400;0,600;1,600&display=swap"
           rel="stylesheet"

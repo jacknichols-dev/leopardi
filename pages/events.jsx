aac96539c6cb2e8d26be9a61aaa3d48349e0a889
jack-dev11 <110835112+jack-dev11@users.noreply.github.com>
2025-01-21 08:28:25 +0000

    title change

--- pages/events.jsx
+++ pages/events.jsx
@@ -24,8 +24,8 @@ const events = () => {
           >
             <div className={styles['events__content--left']}>
               <h1 style={{ textAlign: 'center', paddingBottom: '3rem' }}>
-                Leopardi Occasional Women&apos;s Wear Will Be At These Following
-                Events This Year.
+                Leopardi’s Occasional Women’s Wear will be showcased at the
+                following events this year:
               </h1>
               <p>
                 <span>4th April:</span>4th April: LadyTaverners Luncheon at The
